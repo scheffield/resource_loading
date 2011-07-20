274690edb44baf0c803ceefd9b341c5f5ef32d9c
scheffield <scheffield@scheffield.(none)>
2011-07-20 23:44:05 +0200

    Integrating logging

--- src/main/java/com/scheffield/ria/loading/SlowServingServlet.java
+++ src/main/java/com/scheffield/ria/loading/SlowServingServlet.java
@@ -12,15 +12,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SlowServingServlet extends HttpServlet {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(SlowServingServlet.class);
 
 	private static final String TIME_TO_WAIT_PARAM = "t";
 	private static final String RESOURCE_TO_LOAD = "r";
+
+	private ServletConfig config;
 	
 	@Override
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
+		this.config = config;
 	}
 
 	@Override
@@ -35,6 +42,8 @@ public class SlowServingServlet extends HttpServlet {
 		} catch (InterruptedException e) {
 			throw new ServletException(e);
 		}
+		
+		LOG.info(getServletContext().getRealPath(resource));
 		
 		final File file = new File(resource);
 		FileInputStream fileInputStream = null;
